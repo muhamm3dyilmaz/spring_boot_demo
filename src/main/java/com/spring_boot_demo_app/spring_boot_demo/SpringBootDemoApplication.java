5adb1d7fdb7e8041e3f359d0eb6ad649a2147185
muhamm3dyilmaz <mylmz2101@gmail.com>
2024-09-15 15:03:49 +0300

    SpringBootDemoApplication class updated

--- src/main/java/com/spring_boot_demo_app/spring_boot_demo/SpringBootDemoApplication.java
+++ src/main/java/com/spring_boot_demo_app/spring_boot_demo/SpringBootDemoApplication.java
@@ -4,15 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import com.spring_boot_demo_app.spring_boot_demo.model.Laptop;
+import com.spring_boot_demo_app.spring_boot_demo.service.LaptopService;
+
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(SpringBootDemoApplication.class, args);
 
-		Alien alien = context.getBean(Alien.class);
-		System.out.println("---> Age: " + alien.getAge());
-		alien.code();
+		LaptopService service = context.getBean(LaptopService.class);
+
+		Laptop laptop = context.getBean(Laptop.class);
+		service.addLaptop(laptop);
+
+		// Alien alien = context.getBean(Alien.class);
+		// System.out.println("---> Age: " + alien.getAge());
+		// alien.code();
 	}
 
 }
