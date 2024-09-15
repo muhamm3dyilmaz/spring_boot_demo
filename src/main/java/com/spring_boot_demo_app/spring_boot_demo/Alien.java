be11849563b2eaff1616c824d0341c8c27e34399
muhamm3dyilmaz <mylmz2101@gmail.com>
2024-09-15 13:59:48 +0300

    Alien class updated

--- src/main/java/com/spring_boot_demo_app/spring_boot_demo/Alien.java
+++ src/main/java/com/spring_boot_demo_app/spring_boot_demo/Alien.java
@@ -1,15 +1,36 @@
 package com.spring_boot_demo_app.spring_boot_demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Alien {
 
-    @Autowired
-    Laptop laptop;
+    @Value("22")
+    private int age;
+    private Computer computer;
 
     public void code() {
-        laptop.compile();
+        computer.compile();
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Computer getComputer() {
+        return computer;
+    }
+
+    @Autowired
+    @Qualifier("laptop")
+    public void setComputer(Computer computer) {
+        this.computer = computer;
     }
 }
